0e30087dae1814c47849e555b9c9f72f522acaf5
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-09-01 12:24:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //Initializing an empty set
+  const seenNumbers = new Set();
+  for (const number of array) {
+    const complement = target - number;
+
+    if (seenNumbers.has(complement)) return true;
+
+    seenNumbers.add(number);
+  }
+  return false;
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O: O(n)
 */
 
 /* 
   Add your pseudocode here
+  create an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+  the function iterates over an array of numbers and identifies a complement for each number that adds up to the target.
+  If a complement is found for any of the numbers, it returns true. Otherwise, it returns false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 13, 4, 11, 7], 21));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([4, 8, 12, 4, 11, 7], 16));
+
+  console.log("");
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
